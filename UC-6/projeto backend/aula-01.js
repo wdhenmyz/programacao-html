e8bbaf3d164e17d5053d1ec37d54c2ef4e685010
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-05-08 11:57:49 -0300

    Add files via upload

---
+++ UC-6/projeto backend/aula-01.js
@@ -0,0 +1,129 @@
+//console.log('teste');
+
+//const resultado = 8 > 6
+//const resultado = 8 < 6
+//const resultado = 8 >= 9
+//const resultado = 8 <= 9
+//const resultado = 8 == 8
+//const resultado = 8 == "8"
+//const resultado = 8 === 8
+//const resultado = 8 === "8"
+
+//const resultado = 8 != 8
+//const resultado = 8 != 7
+//const resultado = 8 !== "8"
+
+// const idade = 18;
+// const genero = "Feminino"
+
+// if (idade >= 18 || genero == "Masculino") {
+//     console.log('pode trabalhar');
+// } else {
+//     console.log('não pode trabalhar');
+// }
+
+// idade > 17 ? console.log("sim") : console.log("não");
+
+
+//atividade 01
+// const jogada1 = "pedra"
+// const jogada2 = "tesoura"
+
+// if (jogada1 == "pedra" && jogada2 == "tesoura") {
+//     console.log('pedra');
+// } else {
+//         if (jogada1 == "pedra" && jogada2 == "papel") {
+//             console.log('papel');
+//         } else {
+//             if (jogada1 == "pedra" && jogada2 == "pedra") {
+//                 console.log('empate');
+//             } else {
+//                 if (jogada1 == "papel" && jogada2 == "tesoura") {
+//                     console.log('tesoura');
+//                 } else {
+//                         if (jogada1 == "papel" && jogada2 == "papel") {
+//                             console.log('empate');
+//                         } else {
+//                             if (jogada1 == "papel" && jogada2 == "pedra") {
+//                                 console.log('papel');
+//                             } else {
+//                                 if (jogada1 == "tesoura" && jogada2 == "papel") {
+//                                     console.log('tesoura');
+//                                 } else {
+//                                     if (jogada1 == "tesoura" && jogada2 == "tesoura") {
+//                                         console.log('empate');                                        
+//                                     } else {
+//                                         if (jogada1 == "tesoura" && jogada2 == "pedra") {
+//                                             console.log('pedra');  
+//                                         }
+//                                     }
+//                                 }
+//                             }
+//                         }
+//                     }
+//             }
+//         }
+//     }
+
+
+
+//atividade 02
+
+// const jogador1 = 8;
+// const jogador2 = 3;
+
+// if((jogador1 + jogador2) % 2 === 0) {
+//     console.log("O número é par");
+// } else {
+//     console.log("o número é ímpar");
+// }
+
+
+// atividade 03
+
+// const ladoA = 4;
+// const ladoB = 4;
+
+// if(ladoA === ladoB) {
+//     console.log("SIM");
+// } else {
+//     console.log("NÃO");
+// }
+
+
+// atividade 04
+
+// const alturaEmCm = 185;
+
+// if (alturaEmCm >= 180) {
+//     console.log("aprovado");
+// } else {
+//     console.log("reprovado");
+// }
+
+
+// atividade 05
+
+// const alturaEmCm = 209;
+
+// if (alturaEmCm >= 180 && alturaEmCm <= 185) {
+//     console.log("LÍBERO");
+// } else {
+//     if (alturaEmCm >= 186 && alturaEmCm <= 195) {
+//         console.log("PONTEIRO");
+//     } else {
+//         if (alturaEmCm >= 196 && alturaEmCm <= 205) {
+//             console.log("OPOSTO");
+//         } else {
+//             if (alturaEmCm > 205) {
+//                 console.log("CENTRAL");
+//             } else {
+//                 console.log("REPROVADO");
+//             }
+//         }
+//     }
+// }
+
+
+
+
