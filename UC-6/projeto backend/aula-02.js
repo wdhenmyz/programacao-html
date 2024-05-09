301a89c13a0facc8c876b61002b6a944abff9f8e
wesllen dhenmyz brito ferreira <156432084+wdhenmyz@users.noreply.github.com>
2024-05-09 11:53:42 -0300

    Add files via upload

---
+++ UC-6/projeto backend/aula-02.js
@@ -0,0 +1,121 @@
+// const fruta = "Uva";
+
+// switch (fruta) {
+//     case "Banana":
+//          console.log("Ele gosta de banana.");
+//          break;
+
+//     case "Melancia":
+//          console.log("Ele gosta de melancia.");
+//          break;
+
+//     case "Morango":
+//          console.log("Ele gosta de morango.");
+//          break;
+
+//     default:
+//          console.log("ele não gosta de fruta.");
+//          break;
+// }
+
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+
+// const frutas = ['banana', 'mamao', 'morango', 'limao']
+
+// for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+    
+// }
+
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+
+
+// const numeros = [1, 2, 4, 9, 10]
+
+// for (let i = 0; i < numeros.length; i++) {
+//     // % = resto da divisão
+//     if (numeros[i] %2 == 0) {
+//         //se o resto for zero imprima
+//         console.log(`o numero ${numeros[i]} é par`);
+//     } else {
+//         //se o resto não for zero imprima
+//         console.log(`o numero ${numeros[i]} é ímpar`);
+//     }
+// }
+
+
+// const frutas = ['banana', 'mamao', 'morango', 'limao']
+
+// for (const fruta of frutas) {
+//     console.log(`${fruta}`);
+// }
+
+
+
+
+// Exercício 01
+
+// const numeros = [2, 3, 4, 6];
+// let soma = 0
+
+// for (let i = 0; i < numeros.length; i++) {
+//     soma += numeros[i];
+    
+// }
+// console.log(soma);
+
+
+// exerício 04
+
+// for (let i = 0; i < 500; i++) {
+//     if (i %3 == 0) {
+//         console.log(i);
+//     }
+// }
+
+
+// exercicio 05
+
+// const original = [1, 4, 12, 21, 53, 88, 112];
+// const numerosPares = []
+
+// for (let i = 0; i < original.length; i++) {
+//     // % = resto da divisão
+//     if (original[i] %2 == 0) {
+//         //se o resto for zero imprima
+//         numerosPares.push(original[i]);
+//     }
+// }
+
+// console.log(numerosPares);
+
+
+// exercicio 06
+
+// // Declarar um array com alguns números inteiros
+// const numeros = [3, 4, 7, 8, 1, 6, 5, 10];
+
+// // Variável para armazenar a soma dos números pares
+// let somaPares = 0;
+
+// // Percorrer o array original utilizando um loop "for"
+// for (let i = 0; i < numeros.length; i++) {
+//     // Verificar se o número atual é par
+//     if (numeros[i] % 2 === 0) {
+//         // Se for par, adicionar ao total da soma
+//         somaPares += numeros[i];
+//     }
+// }
+
+// // Imprimir a soma dos números pares no console
+// console.log("A soma dos números pares é:", somaPares);
+
+
+
