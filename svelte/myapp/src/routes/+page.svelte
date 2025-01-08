a0ae5d2c46762ce8e97ccc5ddf90b3190e2641ee
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2025-01-08 14:35:21 +0000

    svelte 1

---
+++ svelte/myapp/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<style>
+    h1 {
+        color: red
+    }
+
+    p {
+        color: blue
+    }
+</style>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
