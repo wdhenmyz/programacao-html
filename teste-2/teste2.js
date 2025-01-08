0f940e6d15f2a7299ae21a26a7d8e29ecd5d6933
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2025-01-08 14:00:01 +0000

    new

---
+++ teste-2/teste2.js
@@ -0,0 +1,19 @@
+import { css } from '@emotion/css'
+
+const color = 'white'
+
+render(
+  <div
+    className={css`
+      padding: 32px;
+      background-color: hotpink;
+      font-size: 24px;
+      border-radius: 4px;
+      &:hover {
+        color: ${color};
+      }
+    `}
+  >
+    Hover to change color.
+  </div>
+)
