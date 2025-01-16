af02091cc7ad5c72aec74c701d585106f5553b3a
wdhenmyz <wesllen.dhenmyz@outlook.com>
2025-01-16 11:45:15 -0300

    WP-1

--- frameworks/webpack/webpack.config.js
+++ frameworks/webpack/webpack.config.js
@@ -1,40 +1,44 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+module.exports = [
+  {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist/WP-1'),
+    }
   },
-};
 
-module.exports = {
-  entry: './src/teste.js',
-  output: {
-    filename: 'teste.js',
-    path: path.resolve(__dirname, 'dist'),
+  {
+    entry: './src/teste.js',
+    output: {
+      filename: 'teste.js',
+      path: path.resolve(__dirname, 'dist/WP-1'),
+    },
   },
-  
-};
 
-module.exports = {
-  entry: './src/teste2.js',
-  output: {
-    filename: 'teste2.js',
-    path: path.resolve(__dirname, 'dist'),
+  {
+    entry: './src/teste2.js',
+    output: {
+      filename: 'teste2.js',
+      path: path.resolve(__dirname, 'dist/WP-1'),
+    },
   },
-};
-module.exports = {
-  entry: './src/teste3.js',
-  output: {
-    filename: 'teste3.js',
-    path: path.resolve(__dirname, 'dist'),
+
+  {
+    entry: './src/teste3.js',
+    output: {
+      filename: 'teste3.js',
+      path: path.resolve(__dirname, 'dist/WP-1'),
+    },
   },
-};
-module.exports = {
-  entry: './src/teste4.js',
-  output: {
-    filename: 'teste4.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+
+  {
+    entry: './src/teste4.js',
+    output: {
+      filename: 'teste4.js',
+      path: path.resolve(__dirname, 'dist/WP-1'),
+    },
+  }
+]
+
