3cea0b1e8d5489b3dc97c818f11a3bc6dd53c59d
wdhenmyz <wesllen.dhenmyz@outlook.com>
2025-01-30 11:50:28 -0300

    indentação

--- frameworks/webpack/src/teste5.jsx
+++ frameworks/webpack/src/teste5.jsx
@@ -33,16 +33,16 @@ hover();
 
 function botao() {
   const Button = styled.button`
-  padding: 32px;
-  background-color: hotpink;
-  font-size: 24px;
-  border-radius: 4px;
-  color: black;
-  font-weight: bold;
-  &:hover {
-    color: white;
-  }
-`;
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    color: black;
+    font-weight: bold;
+    &:hover {
+      color: white;
+    }
+  `;
 
   const App2 = () => <Button>This is my button component.</Button>;
 
